aa167c2598024e989851beefe60a652abfb6205b
omkarg01 <omkargujja01@gmail.com>
2023-08-08 01:23:24 +0530

    login, logout, signup done

--- blog/settings.py
+++ blog/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "bootstrap4",
-    'miniblog'
+    'miniblog',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
--- miniblog/views.py
+++ miniblog/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from miniblog.forms import LoginForm, SignUpForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-
-
 def index(request):
     return render(request, 'miniblog/base.html', {})
 
@@ -14,6 +14,7 @@ def home(request):
 
 
 def logout(request):
+    logout(request)
     return HttpResponseRedirect('/')
 
 
@@ -22,10 +23,32 @@ def dashboard(request):
 
 
 def signup(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'You have successfully signed up!')
+            # login(request, user)
+            # return HttpResponseRedirect('/')
+    else :
+        form = SignUpForm()
     return render(request, 'miniblog/signup.html', {'form': form})
 
 
 def login(request):
-    form = LoginForm()
-    return render(request, 'miniblog/login.html', {'form': form})
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = LoginForm(request, data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate(request, username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in Successfully !!')
+                    return HttpResponseRedirect('/dashboard/')
+        else :
+            form = LoginForm()
+        return render(request, 'miniblog/login.html', {'form': form})
+    else: 
+        return HttpResponseRedirect('/dashboard/')
